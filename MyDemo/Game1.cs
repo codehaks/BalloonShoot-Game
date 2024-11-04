216fd1e86c5338571ecd6af279ad5fe1e3e88e8d
Hakim <ghods.hakim@hotmail.com>
2024-11-04 10:53:07 +0330

    Add project files.

---
+++ MyDemo/Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyDemo
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D _balloonSprite;
+        Texture2D _CrosshairSprite;
+        Texture2D _bgSprite;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _balloonSprite = Content.Load<Texture2D>("assets/balloon");
+          //  _bgSprite = Content.Load<Texture2D>("bg");
+           // _CrosshairSprite = Content.Load<Texture2D>("crosshair");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_balloonSprite, new Rectangle(0, 0,150,150), Color.AliceBlue);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MyDemo/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new MyDemo.Game1();
+game.Run();
